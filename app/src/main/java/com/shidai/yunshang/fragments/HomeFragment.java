760b9e67a8808767f5960f4fc5b694d15137c7de
ruhui <921263933@qq.com>
2017-07-19 01:04:44 +0800

    框架

--- app/src/main/java/com/shidai/yunshang/fragments/HomeFragment.java
+++ app/src/main/java/com/shidai/yunshang/fragments/HomeFragment.java
@@ -2,8 +2,11 @@ package com.shidai.yunshang.fragments;
 
 import com.shidai.yunshang.R;
 import com.shidai.yunshang.fragments.base.BaseFragment;
+import com.shidai.yunshang.view.widget.NavBar;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
 
 /**
  * 创建时间： 2017/7/18.
@@ -14,4 +17,9 @@ import org.androidannotations.annotations.EFragment;
 @EFragment(R.layout.fragment_home)
 public class HomeFragment extends BaseFragment {
 
+    @ViewById(R.id.mNavbar)
+    NavBar mNavbar;
+
+    @AfterViews
+    void initView(){}
 }
